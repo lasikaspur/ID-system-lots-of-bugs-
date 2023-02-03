280358ea23843b0407126719a61a810999d393f6
Johnas Blasco <109265487+lasikaspur@users.noreply.github.com>
2023-02-03 18:08:39 +0800

    Add files via upload

---
+++ password/src/password/Password.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package password;
+
+/**
+ *
+ * @author johna
+ */
+public class Password {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        start begin = new start();
+        begin.show();
+        
+    }
+    
+}
